312334a279d0ffec20eae338f786d429a4c44872
VladSasnyk <sasnykvlad@gmail.com>
2024-05-24 17:33:17 +0300

    upd

--- src/util/loginCheck.js
+++ src/util/loginCheck.js
@@ -2,6 +2,9 @@ export const loginCheck = async (login, password) => {
     console.log(login,password)
     
     // приймає логін і пароль, якщо аккаунт існує має повернути токен чи айді юзера, якщо дані не правильні(аккаунт не зареганий) має повернути false;
+    localStorage.setItem('token', 'сюда токен');
+    
     return 'user-id';
+
     // return false;
 }
